047adaffca2a21d860c8e3cbdbf3f8825165e33e
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2020-07-29 15:08:38 +0300

    minor changes

--- MarsRover/Services/ICoordinateCalculationService.cs
+++ MarsRover/Services/ICoordinateCalculationService.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MarsRover.Models;
 
 namespace MarsRover.Services
 {
     public interface ICoordinateCalculationService
     {
-        
+        void SpinRight(Rover rover);
+        void SpinLeft(Rover rover);
+        void Move(Rover rover);
+        void MoveToFinalLocation(Rover rover, string roverCommand, List<int> upperRightCoordinates);
     }
 }
