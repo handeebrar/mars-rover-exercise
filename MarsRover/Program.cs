53980be8cd64389723a5a1b7a52026af721f25b1
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2020-07-29 14:02:32 +0300

    minor changes about console screen

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using MarsRover.Models;
+using MarsRover.Services;
 
 namespace MarsRover
 {
@@ -14,36 +15,46 @@ namespace MarsRover
                 var upperRightCoordinates = 
                     Console.ReadLine()?.Trim().ToUpper().Split(' ').Select(int.Parse).ToList();
 
-                Console.Write("Initial coordinates of the rover : ");
-                var initialCoordinatesOfTheRover = Console.ReadLine()?.Trim().ToUpper().Split(' ').ToList();
+                while (true)
+                {
+                    Console.Write("Initial coordinates of the rover : ");
+                    var initialCoordinatesOfTheRover = Console.ReadLine()?.Trim().ToUpper().Split(' ').ToList();
 
-                Console.Write("Command string : ");
-                var roverCommand = Console.ReadLine()?.Trim().ToUpper();
+                    Console.Write("Command string : ");
+                    var roverCommand = Console.ReadLine()?.Trim().ToUpper();
 
-                #region InputValidation
+                    #region InputValidation
 
-                if (upperRightCoordinates == null || upperRightCoordinates.Count != 2)
-                    throw new Exception(
-                        "Missing or incorrect format of input : Upper-right coordinates of the plateau.");
+                    if (upperRightCoordinates == null || upperRightCoordinates.Count != 2)
+                        throw new Exception(
+                            "Missing or incorrect format of input : Upper-right coordinates of the plateau.");
 
-                if (initialCoordinatesOfTheRover == null || initialCoordinatesOfTheRover.Count != 3)
-                    throw new Exception("Missing or incorrect format of input : Initial coordinates of the rover.");
+                    if (initialCoordinatesOfTheRover == null || initialCoordinatesOfTheRover.Count != 3)
+                        throw new Exception("Missing or incorrect format of input : Initial coordinates of the rover.");
 
-                if (roverCommand == null)
-                    throw new Exception("Missing or incorrect format of input : The string sent by NASA.");
+                    if (roverCommand == null)
+                        throw new Exception("Missing or incorrect format of input : The string sent by NASA.");
 
-                #endregion
+                    #endregion
 
-                var initialXCoordinate = int.Parse(initialCoordinatesOfTheRover[0]);
-                var initialYCoordinate = int.Parse(initialCoordinatesOfTheRover[1]);
-                var initialDirection = initialCoordinatesOfTheRover[2];
-                var rover =
-                    new Rover(initialXCoordinate, initialYCoordinate, initialDirection);
-                var coordinateCalculationService = new Services.CoordinateCalculationService();
-                coordinateCalculationService.MoveToFinalLocation(rover,roverCommand,upperRightCoordinates);
+                    var initialXCoordinate = int.Parse(initialCoordinatesOfTheRover[0]);
+                    var initialYCoordinate = int.Parse(initialCoordinatesOfTheRover[1]);
+                    var initialDirection = initialCoordinatesOfTheRover[2];
+                    var rover =
+                        new Rover(initialXCoordinate, initialYCoordinate, initialDirection);
+                    var coordinateCalculationService = new CoordinateCalculationService();
+                    coordinateCalculationService.MoveToFinalLocation(rover,roverCommand,upperRightCoordinates);
 
-                Console.WriteLine(
-                    $"Output = {rover.XCoordinate} {rover.YCoordinate} {rover.Direction}");
+                    Console.WriteLine(
+                        $"Output = {rover.XCoordinate} {rover.YCoordinate} {rover.Direction}");
+
+                    Console.Write("Press q to exit / Press any key to continue");
+                    
+                    if (Console.ReadLine() == "q")
+                    {
+                        break;
+                    }
+                }
             }
             catch (Exception exception)
             {
